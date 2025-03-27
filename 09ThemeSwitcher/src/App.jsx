2a10c6607af6e65b70f6512f7a769a95a3fe478b
HarshMane2604 <maneharsh72@gmail.com>
2025-03-27 16:37:17 +0530

    documentation added

--- 09ThemeSwitcher/src/App.jsx
+++ 09ThemeSwitcher/src/App.jsx
@@ -22,7 +22,6 @@ function App() {
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
   
-
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
