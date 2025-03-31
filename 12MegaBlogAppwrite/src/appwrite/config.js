fdfc1198736e35040926047d289d4c6c5793f017
HarshMane2604 <maneharsh72@gmail.com>
2025-03-31 15:35:05 +0530

    learned about create update delete and get via appwrite

---
+++ 12MegaBlogAppwrite/src/appwrite/config.js
@@ -0,0 +1,114 @@
+import conf from '../conf/conf';
+import {Client, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.Client
+            .setEndpoint(conf.aapwriteUrl)
+            .setProject(conf.aapwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost ({title, slug, content, featuredImg, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.aapwriteDatabaseId,
+                conf.aapwriteCollectionId,
+                slug,
+                {
+                    title,
+                    slug, 
+                    content,
+                    featuredImg,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error);
+            
+        }
+    }
+
+    async updatePost( slug,{title, content, featuredImg, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.aapwriteDatabaseId,
+                conf.aapwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImg,
+                    status 
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error);
+            
+        }
+    }
+
+    async deletePost(slug){
+
+        try {
+            await this.databases.deleteDocument(
+                conf.aapwriteDatabaseId,
+                conf.aapwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.aapwriteDatabaseId,
+                conf.aapwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error)
+            return false;
+        }
+    }    
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+             return await this.databases.listDocuments(
+                conf.aapwriteDatabaseId,
+                conf.aapwriteCollectionId,
+                // [
+                //     Query.equal("status", "active")  
+                // ] or as var is declared above
+                queries
+             )
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error);
+            return false;
+        }
+    }
+
+    // FILE UPLOAD SERVICES
+
+    async uploadFile(file){
+        try {
+            
+        } catch (error) {
+            console.log("Appwrite sevice :: getCurrentUser :: error", error);
+            return false;
+        }
+    }
+}
+
+
+const Service = new Service(); 
+export default Service
